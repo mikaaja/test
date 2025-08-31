cab07fa7a78a78fb7867fc84f0302d4407ea8e96
mikaaja <mikaai2213@gmail.com>
2025-09-01 00:36:06 +0700

    Update webhook.js

--- pages/webhook.js
+++ pages/webhook.js
@@ -1,9 +1,8 @@
 export default function handler(req, res) {
   if (req.method !== "POST") return res.status(405).end();
 
-  const { nominal, pesan } = req.body;
+  const payload = req.body;
+  console.log("Webhook Saweria:", payload);
 
-  const qrisUrl = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=Donasi%20${nominal}%20-%20${encodeURIComponent(pesan)}`;
-
-  res.status(200).json({ qrisUrl });
+  res.json({ success: true });
 }
